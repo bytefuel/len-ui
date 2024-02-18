dbb8b8fc862a92fa56de83b7f8a55a76580613b2
hassanusmani21 <91460981+hassanusmani21@users.noreply.github.com>
2024-02-18 12:34:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function App() {
           <Sidebar isSidebar={isSidebar} />
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
+            <h2>dddjdj</h2>
             <Routes>
               {/* <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
